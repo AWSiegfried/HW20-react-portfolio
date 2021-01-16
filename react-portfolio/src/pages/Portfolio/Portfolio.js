34d9ed1c73d756ce41f434d9d975c873ff1d8a0a
AWSiegfried <awsiegfried@gmail.com>
2021-01-15 23:13:15 -0800

    background changes

--- react-portfolio/src/pages/Portfolio/Portfolio.js
+++ react-portfolio/src/pages/Portfolio/Portfolio.js
@@ -1,26 +1,32 @@
-import React from 'react'
-import "./portfolio.css"
-import projects from "./projects.json"
-import Project from "../../components/Project/project"
+import React from "react";
+import "./portfolio.css";
+import projects from "./projects.json";
+import Project from "../../components/Project/project";
 
 export default function Portfolio() {
-    return (
-        <div className="main">
+  return (
+    <div className="main">
+      <div class="row mt-5">
+        <div id="one-section" class="col-12 col-xl-12 pl-xl-n12">
+          <div>
             <h1>Portfolio</h1>
-            <div className = "projects">
-                {projects.map(project => (
-                    <Project
-                    key={project.id}
-                    id={project.id}
-                    name={project.name}
-                    live={project.live}
-                    github={project.github}
-                    imageOne={project.imageOne}
-                    imageTwo={project.imageTwo}
-                    imageThree={project.imageThree}
-                    />
-                ))}
+            <div className="projects">
+              {projects.map((project) => (
+                <Project
+                  key={project.id}
+                  id={project.id}
+                  name={project.name}
+                  live={project.live}
+                  github={project.github}
+                  imageOne={project.imageOne}
+                  imageTwo={project.imageTwo}
+                  imageThree={project.imageThree}
+                />
+              ))}
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
